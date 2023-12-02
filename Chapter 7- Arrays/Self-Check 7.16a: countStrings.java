0e03eb51c2b8e9978c9df8682c03768d09e7a7d2
Gab2125 <152219971+Gab2125@users.noreply.github.com>
2023-12-02 14:07:26 -0500

    Update Self-Check 7.16a: countStrings.java

--- Chapter 7- Arrays/Self-Check 7.16a: countStrings.java
+++ Chapter 7- Arrays/Self-Check 7.16a: countStrings.java
@@ -1,12 +1,12 @@
 /*Write a method countStrings that takes an array of Strings and a target String and returns the number of occurences target appears 
 in the array.*/
 
-public static int countStrings(String[] string, String s) {
-		int i = 0;
-		for(String x : string) {
-			if(x.equals(s)) {
-				i++;
-			}
-		}
-		return i;
-	}
+public static int countStrings(String[] array, String target) {
+        int count = 0;
+        for (String str : array) {
+            if (str.equals(target)) {
+                count++;
+            }
+        }
+        return count;
+}
